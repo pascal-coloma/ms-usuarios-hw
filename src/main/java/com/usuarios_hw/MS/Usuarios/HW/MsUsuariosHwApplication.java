06dfcaeec5b45dfad14a01c67a2846b7a9727095
pascal-coloma <coloma.pascal@gmail.com>
2025-05-08 14:36:35 -0400

    Inicializacion proyecto usuarios

---
+++ src/main/java/com/usuarios_hw/MS/Usuarios/HW/MsUsuariosHwApplication.java
@@ -0,0 +1,13 @@
+package com.usuarios_hw.MS.Usuarios.HW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUsuariosHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUsuariosHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usuarios_hw/MS/Usuarios/HW/MsUsuariosHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuarios_hw.MS.Usuarios.HW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuariosHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

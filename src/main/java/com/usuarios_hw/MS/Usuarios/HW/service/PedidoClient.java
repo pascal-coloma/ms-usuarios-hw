65a2224e8627416faf55cdeea16e5462692b50ac
Pascal Coloma <coloma.pascal@gmail.com>
2025-05-26 23:51:02 -0400

    Implementación docker, despliegue AWS

--- src/main/java/com/usuarios_hw/MS/Usuarios/HW/service/PedidoClient.java
+++ src/main/java/com/usuarios_hw/MS/Usuarios/HW/service/PedidoClient.java
@@ -10,7 +10,7 @@ import java.util.List;
 import com.usuarios_hw.MS.Usuarios.HW.dto.*;
 
 
-@FeignClient(name = "pedido-service", url= "http://localhost:8081/hoppyware/v1/pedido")
+@FeignClient(name = "pedido-service", url= "http://54.165.86.40:8081/hoppyware/v1/pedido")
 public interface PedidoClient {
 
     // Busqueda de pedidos por ID de pedido, a traves del cliente, fijando endpoint al microservicio de pedidos
